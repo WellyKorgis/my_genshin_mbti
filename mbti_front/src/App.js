fd75d3471b5628e762da58b875a16b245524e58a
seu17i <seulgi.lee@integration.works>
2023-05-06 17:52:20 +1200

    Make a survey

--- mbti_front/src/App.js
+++ mbti_front/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+import questionnaireData from './data/questionnaire.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const surveyJson = {
+    title: 'Genshin MBTI Test',
+    showProgressBar: 'bottom',
+    firstPageIsStarted: true,
+    startSurveyText: "Let's do it",
+    pages: [
+      {
+        elements: [
+          {
+            type: 'html',
+            html:
+              'Find your MBTI type and your Genshin character.',
+          },
+        ],
+      },
+      ...Object.entries(questionnaireData).map(([questionId, question]) => ({
+        elements: [
+          {
+            type: 'radiogroup',
+            name: questionId,
+            title: question.text,
+            isRequired: true,
+            choices: Object.entries(question.answers).map(([answerId, answerText]) => ({
+              value: answerId,
+              text: answerText,
+            })),
+          },
+        ],
+      })),
+    ],
+    completedHtml: '<h4>You completed the quiz.</h4>',
+  };
+
+  const survey = new Model(surveyJson);
+
+  return <Survey model={survey} id="surveyContainer" />;
+};
 
 export default App;
